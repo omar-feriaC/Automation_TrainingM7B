7f5e398fda551ce369057c91f3b2a433872393b3
lrodriguez4th <luis.rodriguez@4thsource.com>
2019-12-09 17:14:14 -0600

    Uploading changes

--- AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/IShape.cs
+++ AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/IShape.cs
@@ -8,5 +8,14 @@ namespace AutomationTrainingM7B.M7Exercise
 {
     interface IShape
     {
+        /*ATTRIBUTES*/
+        public static double dbleArea;
+        public static double dblePerimeter;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine(DblArea);
+            Console.WriteLine(DblPerimeter);
+        }
     }
 }
