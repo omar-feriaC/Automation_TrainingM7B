b2b8bcbf5a7991e23927b7efb3dfbecfa46bb640
Aperez11 <alejandro.perez@4thsource.com>
2019-12-09 17:03:42 -0600

    Part of the exercise done

---
+++ AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/2D_Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7B.M7Exercise
+{
+    class _2D_Shape 
+    {
+
+    }
+}
--- AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/IShape.cs
+++ AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/IShape.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace AutomationTrainingM7B.M7Exercise
 {
-    interface IShape
+    interface IShape 
     {
+        //Attributs for Area and Perimeter
+        public static double dbleArea;
+        public static double dblePerimeter;
+
+        //Method to print the Attributes
+        public void DisplayInfo()
+        {
+            Console.WriteLine("This is the area {0} and this is the perimeter {1}", dbleArea, dblePerimeter)
+        }
     }
 }
