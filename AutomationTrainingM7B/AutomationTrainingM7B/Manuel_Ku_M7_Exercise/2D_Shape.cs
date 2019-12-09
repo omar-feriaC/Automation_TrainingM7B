5de48c0b20e945aa6bed6e411eb1e196037d34c2
manuelkumay <manuel.ku@4thsource.com>
2019-12-09 17:07:04 -0600

    Exercise 12-09

---
+++ AutomationTrainingM7B/AutomationTrainingM7B/Manuel_Ku_M7_Exercise/2D_Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7B.Manuel_Ku_M7_Exercise
+{
+    public class _2D_Shape
+    {
+       
+    }
+}
---
+++ AutomationTrainingM7B/AutomationTrainingM7B/Manuel_Ku_M7_Exercise/IShape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7B.Manuel_Ku_M7_Exercise
+{
+    interface IShape 
+    {
+        double Area;
+        double Perimetro;
+    }
+    public void Display()
+    {
+        Console.WriteLine("Print attributes :");
+    }
+         
+}
