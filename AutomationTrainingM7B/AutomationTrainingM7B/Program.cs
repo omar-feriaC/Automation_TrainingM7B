402baf972db1f2f892113b4b7bce2f34d4e3572e
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-12-09 10:33:16 -0600

    Exercise M7

--- AutomationTrainingM7B/AutomationTrainingM7B/Program.cs
+++ AutomationTrainingM7B/AutomationTrainingM7B/Program.cs
@@ -15,50 +15,6 @@ namespace AutomationTrainingM7B
     {
         static void Main(string[] args)
         {
-            Animals objAnimal = new Animals("test1", "test2", "test3", "test4");
-            objAnimal.fnEating();
-            objAnimal.fnMovement();
-            objAnimal.fnSound();
-
-            Console.WriteLine("-------------------------");
-
-            Dog objDog = new Dog();
-            objDog.fnEating();
-            objDog.fnMovement();
-            objDog.fnSound();
-            objDog.fnPrintRaza();
-
-            /*SetupDriver();
-            
-            LoginPage_Mercury objLogin = new LoginPage_Mercury(objDriver);
-            LoginPage_Mercury.fnEnterUserNameTxt(strUserName);
-            LoginPage_Mercury.fnEnterPasswordTxt(strPassword);
-            LoginPage_Mercury.fnClickSignInBtn();
-
-            ExitDriver();*/
-
-            /*objDriver = new ChromeDriver();
-            objDriver.Url = strUrl;
-            Object Page
-            objDriver.FindElement(By.Id("userName")).Clear();
-            objDriver.FindElement(By.Name("userName")).SendKeys(strUserName);
-            objDriver.FindElement(By.Name("password")).Clear();
-            objDriver.FindElement(By.Name("password")).SendKeys(strPassword);
-
-            objDriver.FindElement(By.Name("userName")).Clear();
-            objDriver.FindElement(By.Name("userName")).SendKeys("admin123");
-            objDriver.FindElement(By.Name("password")).Clear();
-            objDriver.FindElement(By.Name("password")).SendKeys("5464564");
-
-            objDriver.FindElement(By.XPath("//input[@name='login']")).Click();
-            objDriver.FindElement(By.LinkText("REGISTER")).Click();
-
-
-            //Driver Kill
-            objDriver.Close();
-            objDriver.Quit();
-            */
-
             Console.ReadKey();
         }
     }
