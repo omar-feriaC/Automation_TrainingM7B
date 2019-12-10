35adde8c6fa63b882887b328a3d41380e1c93adc
Aperez11 <alejandro.perez@4thsource.com>
2019-12-10 16:38:21 -0600

    Part of the exercise done 2

--- AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/2D_Shape.cs
+++ AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/2D_Shape.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace AutomationTrainingM7B.M7Exercise
 {
-    class _2D_Shape 
+    class _2D_Shape:IShape
+
     {
+        public double dbleArea { get; set; }
+        public double dblPerimeter { get; set; }
+
+        
+
+        //METHOD FOR DISPLAYING INFORMATION
+        public void DisplayInfo()
+        {
+            Console.WriteLine("This is the area {0} and this is the perimeter {1}", dbleArea, dblPerimeter);
+        }
+
+        //INITIALIZE ATTRIBUTES
+        public _2D_Shape()
+        {
+            string strName = "";
+            double dbleArea = 0;
+            double dblPerimeter = 0;
+        }
 
     }
 }
--- AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/IShape.cs
+++ AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/IShape.cs
@@ -8,14 +8,10 @@ namespace AutomationTrainingM7B.M7Exercise
 {
     interface IShape 
     {
-        //Attributs for Area and Perimeter
-        public static double dbleArea;
-        public static double dblePerimeter;
+        //ATTRIBUTES
+         double dbleArea { get; set; }
+         double dblPerimeter { get; set; }
 
-        //Method to print the Attributes
-        public void DisplayInfo()
-        {
-            Console.WriteLine("This is the area {0} and this is the perimeter {1}", dbleArea, dblePerimeter)
-        }
+    void DisplayInfo();
     }
 }
