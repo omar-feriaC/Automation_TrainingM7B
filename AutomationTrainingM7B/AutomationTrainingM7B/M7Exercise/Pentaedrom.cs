2d996d3745e0cfdf9123f108cea6f946d9a14406
Aperez11 <alejandro.perez@4thsource.com>
2019-12-10 16:48:11 -0600

    Part of the exercise done 2

---
+++ AutomationTrainingM7B/AutomationTrainingM7B/M7Exercise/Pentaedrom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7B.M7Exercise
+{
+    class Pentaedrom : _3D_Shape
+    {
+    }
+}
