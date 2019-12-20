5f075fa905ad995c2be4f1af158d56fb4c74d115
Jose Humberto Cruz Valencia <jose.cruz@4thsource.com>
2019-12-20 10:15:26 -0600

    Adding SearchPage POM

---
+++ AutomationTrainingM7B/AutomationTrainingM7B/Page Objects/M8LinkedIn_SearchPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTrainingM7B.Page_Objects
+{
+    class M8LinkedIn_SearchPage
+    {
+    }
+}
